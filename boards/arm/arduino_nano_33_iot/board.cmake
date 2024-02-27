b342d984ad9ab21b3fe30475c253bad92b28769b
Ivan <146200551+IvanVeloz@users.noreply.github.com>
2024-02-27 10:16:12 -0500

    Added pyOCD includes and arguments
    This is to enable debugging support to the board via pyOCD

--- boards/arm/arduino_nano_33_iot/board.cmake
+++ boards/arm/arduino_nano_33_iot/board.cmake
@@ -1,4 +1,6 @@
 # Copyright (c) 2020 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(pyocd "--target=atsamd21g18a" "--frequency=4000000")
 include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
